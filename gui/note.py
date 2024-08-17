cdb87d8cea65afe2b5574e0cac72253523a97741
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-17 19:32:55 +0200

    feat: added code to save_note

--- gui/note.py
+++ gui/note.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from . import start
+from core import logic
 
 
 class NoteScreen(tk.Frame):
     def __init__(self, master, note_text: str):
         super().__init__(master=master)
+        self.textbox = None
         self.note_text = note_text
         self.header()
         self.note_textbox()
@@ -30,22 +32,23 @@ class NoteScreen(tk.Frame):
             text='Save Note',
             font=('Arial', 16),
             height=1,
-            width=10
+            width=10,
+            command=self.save_note
         )
         save_button.grid(row=0, column=1, sticky='w', padx=2.5)
 
         button_frame.grid(row=0, column=0, sticky='w')
 
     def note_textbox(self):
-        textbox = tk.Text(
+        self.textbox = tk.Text(
             self,
             font=('Arial', 12),
             wrap='word',
             width=200
         )
-        textbox.insert(tk.END, self.note_text)
+        self.textbox.insert(tk.END, self.note_text)
 
-        textbox.grid(row=1, column=0, padx=20, pady=10, sticky='nsew')
+        self.textbox.grid(row=1, column=0, padx=20, pady=10, sticky='nsew')
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
@@ -54,3 +57,7 @@ class NoteScreen(tk.Frame):
 
         start_screen = start.StartScreen(self.master)
         start_screen.pack(fill='both', expand=True)
+
+    def save_note(self):
+        note_content = self.textbox
+        logic.save_note('notiz.txt', text=note_content.get('1.0', tk.END).strip())
