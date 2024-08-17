8b0226b01eba52bbdffef9fe5d89e1c8c26dadbd
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-17 19:19:42 +0200

    feat: button_frame

--- gui/note.py
+++ gui/note.py
@@ -10,16 +10,31 @@ class NoteScreen(tk.Frame):
         self.note_textbox()
 
     def header(self):
+        button_frame = tk.Frame(self)
+        button_frame.columnconfigure(0, weight=1)
+        button_frame.columnconfigure(1, weight=1)
+        button_frame.columnconfigure(2, weight=1)
+
         back_btn = tk.Button(
-            self,
+            button_frame,
             text='Back',
             font=('Arial', 16),
             height=1,
             width=5,
             command=self.back_to_start
         )
-        back_btn.grid(row=0, column=0, sticky='w')
-        self.grid_columnconfigure(0, weight=1)
+        back_btn.grid(row=0, column=0, sticky='w', padx=2.5)
+
+        save_button = tk.Button(
+            button_frame,
+            text='Save Note',
+            font=('Arial', 16),
+            height=1,
+            width=10
+        )
+        save_button.grid(row=0, column=1, sticky='w', padx=2.5)
+
+        button_frame.grid(row=0, column=0, sticky='w')
 
     def note_textbox(self):
         textbox = tk.Text(
