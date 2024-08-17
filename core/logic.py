69a1d2f2051121871639e0a3cfe1f5370025f3ea
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-17 19:50:46 +0200

    feat: saving note

--- core/logic.py
+++ core/logic.py
@@ -1,2 +1,8 @@
-def new_note(path: str):
+def save_note(path: str, text):
+    with open(path, 'w') as file:
+        file.write(text)
+        file.close()
+
+
+def open_note(path: str):
     pass
--- gui/note.py
+++ gui/note.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from . import start
 from core import logic
+from tkinter import messagebox, filedialog
 
 
 class NoteScreen(tk.Frame):
@@ -59,5 +60,35 @@ class NoteScreen(tk.Frame):
         start_screen.pack(fill='both', expand=True)
 
     def save_note(self):
-        note_content = self.textbox
-        logic.save_note('notiz.txt', text=note_content.get('1.0', tk.END).strip())
+        folder_selected = filedialog.askdirectory(title='Select Folder')
+
+        if folder_selected:
+            self.enter_filename(folder_selected=folder_selected)
+
+            print(folder_selected)
+
+    def enter_filename(self, folder_selected):
+        def on_confirm():
+            note_content = self.textbox
+            filename = entry.get()
+            if filename:
+                logic.save_note(f'{folder_selected}/{filename}.txt', text=note_content.get('1.0', tk.END).strip())
+                messagebox.showinfo('Saved Note', 'Note has been saved successfully')
+            else:
+                messagebox.showinfo('Input Error', 'Please enter a filename')
+
+        def on_cancel():
+            top.destroy()
+
+        top = tk.Toplevel(self)
+        top.title("Enter Filename")
+
+        tk.Label(top, text="Filename:").pack(pady=5)
+        entry = tk.Entry(top, width=50)
+        entry.pack(pady=5)
+
+        btn_frame = tk.Frame(top)
+        btn_frame.pack(pady=5)
+
+        tk.Button(btn_frame, text="Confirm", command=on_confirm).pack(side=tk.LEFT, padx=5)
+        tk.Button(btn_frame, text="Cancel", command=on_cancel).pack(side=tk.LEFT, padx=5)
