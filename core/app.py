377ac5e8d1205465096a0f6768a3d04ae898ad1d
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-18 18:56:22 +0200

    feat: made tk to ctk & removed version

--- core/app.py
+++ core/app.py
@@ -1,17 +1,16 @@
-import tkinter as tk
+import customtkinter as ctk
 from gui import start
 
 
-class App(tk.Tk):
+class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.version = '0.3'
         self.settings()
         self.show_frame(start.StartScreen)
 
     def settings(self):
         self.geometry('800x500')
-        self.title(f'SoulEdit - {self.version}')
+        self.title(f'SoulEdit')
 
     def show_frame(self, frame_class):
         frame = frame_class(self)
