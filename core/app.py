7ef2d9095cf3723ecc1ed0440b02d8fb41ede1d4
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2024-08-18 09:47:34 +0200

    rename

--- core/app.py
+++ core/app.py
@@ -5,13 +5,13 @@ from gui import start
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.version = '0.2'
+        self.version = '0.3'
         self.settings()
         self.show_frame(start.StartScreen)
 
     def settings(self):
         self.geometry('800x500')
-        self.title(f'SoulNote - {self.version}')
+        self.title(f'SoulEdit - {self.version}')
 
     def show_frame(self, frame_class):
         frame = frame_class(self)
